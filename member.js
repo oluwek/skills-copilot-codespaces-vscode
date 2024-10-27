85a44c5f7ff3957a6c1d7390547cefd2f5db8871
oluwek <30313537+oluwek@users.noreply.github.com>
2024-10-27 21:09:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMembers() {
+    return [
+        {
+            name: 'Jenny',
+            skills: ['JavaScript', 'HTML', 'CSS']
+        },
+        {
+            name: 'Jenny',
+            skills: ['Python', 'Java', 'C++']
+        },
+        {
+            name: 'Jenny',
+            skills: ['Ruby', 'C#', 'C']
+        }
+    ];
+}
